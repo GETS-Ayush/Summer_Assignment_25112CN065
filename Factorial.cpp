55aa6b5aa2eba2104e7d39ae638875046fc96117
GETS-Ayush <ayushvermaer@gmail.com>
2026-05-22 20:25:02 +0530

    Day_01

---
+++ Factorial.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,fac=1;
+    cout<<"Enter a number:";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        fac=fac*i;
+    }
+    cout<<"Factorial="<<fac;
+    return 0;
+}
